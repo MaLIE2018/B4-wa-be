6137f11dba98e64ec446edba72683e736eb545f9
MaLIE2018 <liebsch@pm.me>
2021-07-20 13:05:29 +0200

    update

--- build/lib/auth/auth.js
+++ build/lib/auth/auth.js
@@ -8,106 +8,11 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-<<<<<<< HEAD
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-=======
->>>>>>> developement
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.basicAuthMiddleware = exports.JWTMiddleWare = void 0;
-<<<<<<< HEAD
-var userSchema_1 = __importDefault(require("../../services/user/userSchema"));
-var http_errors_1 = __importDefault(require("http-errors"));
-var tools_1 = require("./tools");
-var atob_1 = __importDefault(require("atob"));
-var JWTMiddleWare = function (req, res, next) { return __awaiter(void 0, void 0, void 0, function () {
-    var content, user, error_1;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0:
-                if (!!req.cookies.access_token) return [3 /*break*/, 1];
-                next(http_errors_1.default(401, { message: "Provide Access Token" }));
-                return [3 /*break*/, 5];
-            case 1:
-                _a.trys.push([1, 4, , 5]);
-                return [4 /*yield*/, tools_1.verifyAccessToken(req.cookies.access_token)];
-            case 2:
-                content = _a.sent();
-                return [4 /*yield*/, userSchema_1.default.findById(content._id)];
-            case 3:
-                user = _a.sent();
-                if (user) {
-                    req.user = user;
-                    next();
-                }
-                else {
-                    next(http_errors_1.default(404, { message: "User not found" }));
-                }
-                return [3 /*break*/, 5];
-            case 4:
-                error_1 = _a.sent();
-                next(http_errors_1.default(401, { message: "AccessToken not valid" }));
-                return [3 /*break*/, 5];
-            case 5: return [2 /*return*/];
-        }
-    });
-}); };
-exports.JWTMiddleWare = JWTMiddleWare;
-var basicAuthMiddleware = function (req, res, next) { return __awaiter(void 0, void 0, void 0, function () {
-    var decoded, _a, email, password, user;
-    return __generator(this, function (_b) {
-        switch (_b.label) {
-            case 0:
-                if (!!req.headers.authorization) return [3 /*break*/, 1];
-                next(http_errors_1.default(401, { message: "Authorization required" }));
-                return [3 /*break*/, 3];
-            case 1:
-                decoded = atob_1.default(req.headers.authorization.split(" ")[1]);
-                _a = decoded.split(":"), email = _a[0], password = _a[1];
-                return [4 /*yield*/, userSchema_1.default.checkCredentials(email, password)];
-            case 2:
-                user = _b.sent();
-                if (user) {
-                    req.user = user;
-                    next();
-                }
-                else {
-                    next(http_errors_1.default(401, { message: "Credentials wrong" }));
-                }
-                _b.label = 3;
-            case 3: return [2 /*return*/];
-        }
-    });
-}); };
-=======
 const userSchema_1 = __importDefault(require("../../services/user/userSchema"));
 const http_errors_1 = __importDefault(require("http-errors"));
 const tools_1 = require("./tools");
@@ -119,7 +24,9 @@ const JWTMiddleWare = (req, res, next) => __awaiter(void 0, void 0, void 0, func
     else {
         try {
             const content = yield tools_1.verifyAccessToken(req.cookies.access_token);
-            const user = yield userSchema_1.default.findById(content._id);
+            const user = yield userSchema_1.default.findById(content._id).populate("friends", {
+                profile: 1,
+            });
             if (user) {
                 req.user = user;
                 next();
@@ -151,5 +58,4 @@ const basicAuthMiddleware = (req, res, next) => __awaiter(void 0, void 0, void 0
         }
     }
 });
->>>>>>> developement
 exports.basicAuthMiddleware = basicAuthMiddleware;
--- build/lib/auth/oauth.js
+++ build/lib/auth/oauth.js
@@ -1,2 +1,59 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const passport_1 = __importDefault(require("passport"));
+const userSchema_1 = __importDefault(require("../../services/user/userSchema"));
+const tools_1 = require("./tools");
+const passport_google_oauth20_1 = require("passport-google-oauth20");
+let GoogleStrategy;
+if (process.env.GOOGLE_ID !== undefined &&
+    process.env.GOOGLE_SECRET !== undefined) {
+    GoogleStrategy = new passport_google_oauth20_1.Strategy({
+        clientID: process.env.GOOGLE_ID,
+        clientSecret: process.env.GOOGLE_SECRET,
+        callbackURL: process.env.FE_URL + "/users/googleRedirect",
+    }, (accessToken, refreshToken, profile, passportNext) => __awaiter(void 0, void 0, void 0, function* () {
+        try {
+            console.log("profile:", profile);
+            const user = yield userSchema_1.default.findOne({ googleId: profile.id });
+            if (user) {
+                const tokens = yield tools_1.JWTAuthenticate(user);
+                passportNext(null, { user, tokens });
+            }
+            else {
+                const newUser = {
+                    profile: {
+                        firstName: profile.name.givenName,
+                        lastName: profile.name.lastName,
+                        email: profile.emails[0].value,
+                        googleId: profile.id,
+                    },
+                };
+                const createdUser = new userSchema_1.default(newUser);
+                const savedUser = yield createdUser.save();
+                const tokens = yield tools_1.JWTAuthenticate(savedUser);
+                passportNext(null, { user: savedUser, tokens });
+            }
+        }
+        catch (error) {
+            passportNext(error);
+        }
+    }));
+    passport_1.default.use("google", GoogleStrategy);
+}
+passport_1.default.serializeUser(function (user, passportNext) {
+    // this is for req.user
+    passportNext(null, user);
+});
+exports.default = {};
