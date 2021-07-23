62a8f4bed7d4f75270debc3200d65271000cd204
MaLIE2018 <liebsch@pm.me>
2021-07-23 15:59:39 +0200

    build

--- build/app.js
+++ build/app.js
@@ -10,27 +10,29 @@ const errorhandler_1 = __importDefault(require("./lib/errorhandler"));
 const http_1 = require("http");
 const services_1 = __importDefault(require("./services"));
 const passport_1 = __importDefault(require("passport"));
+const http_errors_1 = __importDefault(require("http-errors"));
 const app = express_1.default();
-// const originsURLS = [
-//   process.env.FE_URL,
-//   process.env.FE_DEV_URL,
-//   process.env.FE_GOOGLE_URL,
-//   process.env.FE_GOOGLE_REDIRECT_URL,
-//   undefined,
-// ];
-// const corsOptions = {
-//   origin: function (origin: any, next: any) {
-//     if (originsURLS.indexOf(origin) !== -1) {
-//       console.log("origin:", origin);
-//       next(null, true);
-//     } else {
-//       console.log("origin403:", origin);
-//       next(createError(403, { message: "Check your cors settings!" }));
-//     }
-//   },
-//   credentials: true,
-// };
-app.use(cors_1.default());
+const originsURLS = [
+    process.env.FE_URL,
+    process.env.FE_DEV_URL,
+    process.env.FE_GOOGLE_URL,
+    process.env.FE_GOOGLE_REDIRECT_URL,
+    undefined,
+];
+const corsOptions = {
+    origin: function (origin, next) {
+        if (originsURLS.indexOf(origin) !== -1) {
+            console.log("origin:", origin);
+            next(null, true);
+        }
+        else {
+            console.log("origin403:", origin);
+            next(http_errors_1.default(403, { message: "Check your cors settings!" }));
+        }
+    },
+    credentials: true,
+};
+app.use(cors_1.default(corsOptions));
 app.use(express_1.default.json());
 app.use(cookie_parser_1.default());
 app.use(passport_1.default.initialize());
